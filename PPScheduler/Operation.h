f7482a7c6a0b7689c9806d243c94bcc4bb935f4d
Unknown <maeglin64@gmail.com>
2018-03-05 18:41:08 +0100

    nie wiem co xD

--- PPScheduler/Operation.h
+++ PPScheduler/Operation.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "Order.h"
+#include "WorkCenter.h"
 using namespace std;
 class Operation :
 	public Order
+	
 {
 public:
+	WorkCenter * workcenter;
 	Operation();
 	~Operation();
 };
